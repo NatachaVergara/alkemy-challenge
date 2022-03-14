73e9f98e68dda91d77bc2b9fae82b45e7b7da982
Natacha Vergara <ntchvergara@gmail.com>
2022-03-14 16:19:35 -0300

    actualizando el apikey

--- src/Hooks/UseApi.jsx
+++ src/Hooks/UseApi.jsx
@@ -5,10 +5,16 @@ import axios from 'axios'
 
 
 const UseApi = async ({ query, setRecetas }) => {
-//const api1y = `24f235a649d44e499d188ea2acc82e7d`
- const ap2g = 'bcdb654fd44d4a9692e33796af4230b4'
+  //ntch yahoo
+  const api1y = `24f235a649d44e499d188ea2acc82e7d`
+  
+  //ntch gmail
+  //const ap2g = 'bcdb654fd44d4a9692e33796af4230b4'
+  
+  //todo.hotmail
+   const api3 = 'a2ff818ee82c4b92b30209b18a1327fd'  
 
-  const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${ap2g}&query=${query}&addRecipeInformation=true&number=50`
+  const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${api3}&query=${query}&addRecipeInformation=true&number=50`
 
   //const { setApiSearch} = useMenuContext()
 
