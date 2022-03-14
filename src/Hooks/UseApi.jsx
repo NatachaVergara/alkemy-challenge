ed86fdca8f9841e7b0cfac03440f3bc051a396fb
Natacha Vergara <ntchvergara@gmail.com>
2022-03-14 12:05:48 -0300

    Tratando de terminar el css responsive

--- src/Hooks/UseApi.jsx
+++ src/Hooks/UseApi.jsx
@@ -5,7 +5,7 @@ import axios from 'axios'
 
 
 const UseApi = async ({ query, setRecetas }) => {
-  const api1y = `24f235a649d44e499d188ea2acc82e7d`
+ // const api1y = `24f235a649d44e499d188ea2acc82e7d`
   const ap2g = 'bcdb654fd44d4a9692e33796af4230b4'
 
   const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${ap2g}&query=${query}&addRecipeInformation=true&number=50`
